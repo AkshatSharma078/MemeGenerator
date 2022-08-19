2bc89f8a409b068d2dadc163f89d1dab16bad755
akshat <akshat974321@gmail.com>
2022-08-19 19:18:15 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./components/New"
+ import Body from "./components/Body"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Body/>
+        </div>
+    )
+}
+
